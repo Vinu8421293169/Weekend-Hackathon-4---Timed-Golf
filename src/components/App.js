17005543bed93e4eb7d6b42a5909d7844a761662
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-09 12:07:26 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,49 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-  }
-  componentDidMount() {
-    
-  }
+const App = () => {
+  let x = 0;
+  let y = 0;
+  let interval;
+  const [time, setTime] = useState(0);
+  const [ballPosition, setBallPosition] = useState({ top: x, left: y });
 
-  componentWillUnmount() {
-    
-  }
+  const handleKeydown = (el) => {
+    if (el.keyCode === 37) y -= 5;
+    if (el.keyCode === 38) x -= 5;
+    if (el.keyCode === 39) y += 5;
+    if (el.keyCode === 40) x += 5;
 
+    setBallPosition({ top: x, left: y });
+    if (x === 250 && y === 250) {
+      clearInterval(interval);
+    }
+  };
 
+  const startGame = () => {
+    interval = setInterval(() => setTime(time + 1), 1000);
+    window.addEventListener("keydown", (el) => handleKeydown(el));
+  };
 
-  render() {
-    return (
- <>
-</>
-    );
-  }
-}
-
-export default Timer;
+  return (
+    <>
+      <div
+        className="ball"
+        style={{
+          position: "absolute",
+          top: `${ballPosition.top}px`,
+          left: `${ballPosition.left}px`
+        }}
+      />
+      <div className="hole" />
+      <div className="heading-timer">{time}</div>
+      <button
+        className="button"
+        style={{ position: "absolute", top: "250px", right: "250px" }}
+        onClick={startGame}
+      >
+        Start
+      </button>
+    </>
+  );
+};
+export default App;
